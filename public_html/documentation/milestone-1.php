b0e938fa05f38d98eea1d3f435f8f6c2ab54b09e
Floribella Ponce <fponce2@cnm.edu>
2019-10-17 22:38:51 -0600

    adding persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -16,5 +16,20 @@ business owner/personal brand that is looking for creative services such as vide
 <h3>Goal</h3>
 The goal is to promote the services of Jungle Cat Media through the website so that
 they can reach potential clients and take care of any of their videography needs.
+<h3>Persona</h3>
+<strong>Name: </strong>Edwin Martinez
+<strong>Age: </strong> 29
+<strong>Profession: </strong>Owner of Slaytanic, a personal brand of merchandise consisting of clothing and
+accessories. He is looking to promote his product in order to reach a larger audience.
+<strong>Lifestyle: </strong> Edwin spends the majority of his time running his business. 
+<strong>Frustration and Needs: </strong> Edwin needs to expand his customer base. He has engaged in marketing
+strategies such as starting a social media account for his brand and showing his product to other businesses
+but it is has not been efficient for him. Edwin is new to the business of sales and marketing and is needing
+an easier way to get his product known. Edwin is running his business solo.
+<strong>Goals: </strong> Edwin is looking for a more effective marketing strategy to reach a larger
+population. Edwin is wanting to engage users in the easiest way possible to get customers
+intrigued in his product.
+<strong>User Story: </strong>As a founder of a personal brand, I need a to showcase my brand through
+video to reach a larger audience in order to increase my sales.
 </body>
 </html>
