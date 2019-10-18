5c3615eea8f54ff26de6a77919d1800370ad1cfa
Floribella Ponce <fponce2@cnm.edu>
2019-10-17 21:54:24 -0600

    added purpose, audience, and goal

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,12 +5,16 @@
 	<title>Milestone 1</title>
 </head>
 <body>
+<h1 align="center">PWP Milestone 1</h1>
+<h2 align="center">Jungle Cat Media</h2>
 <h3>Purpose</h3>
-
+The purpose of this website will be to showcase Jungle Cat Media's work as a portfolio and also
+to provide information on how to contact Jungle Cat Media for any inquiries and requests.
 <h3>Audience</h3>
-
+The type of audience targeted are business owners and personal brands. The focus is to reach any
+business owner/personal brand that is looking for creative services such as videography.
 <h3>Goal</h3>
-
-<h3>Persona</h3>
+The goal is to promote the services of Jungle Cat Media through the website so that
+they can reach potential clients and take care of any of their videography needs.
 </body>
 </html>
