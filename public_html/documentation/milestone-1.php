f8fd4aae54cf1edb836945bd6451efa80da5dcfa
Floribella Ponce <fponce2@cnm.edu>
2019-10-17 21:28:01 -0600

    added doctype layout

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Milestone 1</title>
+</head>
+<body>
+<h3>Purpose</h3>
+
+<h3>Audience</h3>
+
+<h3>Goal</h3>
+
+<h3>Persona</h3>
+</body>
+</html>
