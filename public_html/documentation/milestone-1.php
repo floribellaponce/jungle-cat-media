415a0c2652e9d34c2964b7bc278f4f5a3bd8c61a
Floribella Ponce <fponce2@cnm.edu>
2019-10-18 00:05:22 -0600

    grammer revision

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,46 +9,46 @@
 <h2 align="center">Jungle Cat Media</h2>
 <h3>Purpose</h3>
 The purpose of this website will be to showcase Jungle Cat Media's work as a portfolio and also
-to provide information on how to contact Jungle Cat Media for any inquiries and requests.
+to provide a contact method for any inquiries and requests.
 <h3>Audience</h3>
-The type of audience targeted are business owners and personal brands. The focus is to reach any
+The target audience are business owners and personal brands. The focus is to reach any
 business owner/personal brand that is looking for creative services such as videography.
 <h3>Goal</h3>
-The goal is to promote the services of Jungle Cat Media through the website so that
-they can reach potential clients and take care of any of their videography needs.
+The goal is to promote Jungle Cat Media' services through the website so that
+they can reach potential clients and take care of all their videography needs.
 <h3>Persona</h3>
 <p>
 	<strong>Name: </strong>Edwin Martinez
 </p>
 <p>
-	<strong>Age: </strong> 29
+	<strong>Age: </strong> 25
 </p>
 <p>
 	<strong>Profession: </strong>Owner of Slaytanic, a personal brand of merchandise consisting of clothing and
-	accessories. He is looking to promote his product in order to reach a larger audience.
+	accessories. Edwin personally creates the graphic designs of his merchandise, manages the sales, and is responsible
+	for the shipping and fulfillment.
 </p>
 <p>
-	<strong>Lifestyle: </strong> Edwin is a single father of a 5 year old child. He has background in graphic design
-	and has taken a few courses in entrepreneurship. He started a solo personal brand a few months ago. He personally
-	creates the graphics on his merchandise, manages the sales, and is responsible for the shipping and fulfillment.
-	When he is not managing his business he spends time with his child and painting new artwork.
+	<strong>Lifestyle: </strong> Edwin is a single father of a 5 year old child. He has a deep passion for his artwork
+	and custom clothing. He has background in graphic design and has taken a few courses in entrepreneurship. He launched
+	his solo personal brand a few months ago. When he is not managing his business he spends time with his child and
+	painting new artwork.
 </p>
 <p>
 	<strong>Technology: </strong> Uses Adope Photoshop platform and social media on MacBook, iPad, and iPhone.
 </p>
 <p>
 	<strong>Frustrations and Needs: </strong>Edwin needs to expand his customer base. He has engaged in marketing
-	strategies such as, starting a social media account for his brand, where he posts images of his products with short
+	strategies such as, social media account for his brand, where he posts images of his products with short
 	descriptions but it is has not been efficient for him. Edwin is new to the business of sales and marketing
-	and is needing an easier way to get his product known.
+	and is needing an easier and cost efficient way to get his product known without having.
 </p>
 <p>
 	<strong>Goals: </strong>Edwin's desire is to get people intrigued in his product in the easiest way possible
 	in hopes of expanding his customer range. He wants to find an efficient marketing service that will allow him
 	to have more time to focus on his artwork and personal brand.
-<p>
-	<strong>User Story: </strong>As a founder of a personal brand, I need a to showcase my brand through
-	video to reach a larger audience in order to increase my sales.
 </p>
+<h3>User Story</h3>As a founder of a personal brand, I need a to showcase my brand through
+	video to reach a larger audience in order to increase my sales.
 </body>
 </html>
