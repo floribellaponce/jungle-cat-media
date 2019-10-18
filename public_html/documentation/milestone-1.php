56f9a160df9e5ca42f70ca4e2556f51b14887ef7
Floribella Ponce <fponce2@cnm.edu>
2019-10-17 23:27:10 -0600

    input technology info

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -17,31 +17,38 @@ business owner/personal brand that is looking for creative services such as vide
 The goal is to promote the services of Jungle Cat Media through the website so that
 they can reach potential clients and take care of any of their videography needs.
 <h3>Persona</h3>
-<strong>Name: </strong>Edwin Martinez
-<strong>Age: </strong> 29
-<strong>Profession: </strong>Owner of Slaytanic, a personal brand of merchandise consisting of clothing and
-accessories. He is looking to promote his product in order to reach a larger audience.
-<strong>Lifestyle: </strong> Edwin is a single father of a 5 year old child. He has background in graphic design
-and has taken a few courses in entrepreneurship. He started a solo personal brand a few months ago. He personally
-creates the graphics on his merchandise, manages the sales, and is responsible for the shipping and fulfillment.
-When he is not managing his business he spends time with his child and painting new artwork.
-<strong>Frustrations: </strong>
-<ul>
-	<<li>Hours spent on inefficient marketing techniques</li>
-	<li>Not having enough time to focus on creating new content for his personal brand</li>
-	<li>Customer base is not large enough to be substancial </li>
-		Edwin needs to expand his customer base. He has engaged in marketing
-strategies such as starting a social media account for his brand and showing his product to other businesses
-but it is has not been efficient for him. Edwin is new to the business of sales and marketing and is needing
-an easier way to get his product known. Edwin is running his business solo.
-<strong>Goals: </strong>
-	<ul>
-		<li>Reach a wider customer range </li>
-		<li>Get people intrigued in his product in the easier way possible </li>
-		<li>Find an efficient marketing tool</li>
-		<li>Have more time to create more content</li>
-	</ul>
-<strong>User Story: </strong>As a founder of a personal brand, I need a to showcase my brand through
-video to reach a larger audience in order to increase my sales.
+<p>
+	<strong>Name: </strong>Edwin Martinez
+</p>
+<p>
+	<strong>Age: </strong> 29
+</p>
+<p>
+	<strong>Profession: </strong>Owner of Slaytanic, a personal brand of merchandise consisting of clothing and
+	accessories. He is looking to promote his product in order to reach a larger audience.
+</p>
+<p>
+	<strong>Lifestyle: </strong> Edwin is a single father of a 5 year old child. He has background in graphic design
+	and has taken a few courses in entrepreneurship. He started a solo personal brand a few months ago. He personally
+	creates the graphics on his merchandise, manages the sales, and is responsible for the shipping and fulfillment.
+	When he is not managing his business he spends time with his child and painting new artwork.
+</p>
+<p>
+	<strong>Technology: </strong> Uses Adope Photoshop platform and social media on MacBook, iPad, and iPhone.
+</p>
+<p>
+	<strong>Frustrations and Needs: </strong>Edwin needs to expand his customer base. He has engaged in marketing
+	strategies such as, starting a social media account for his brand, where he posts images of his products with short
+	descriptions but it is has not been efficient for him. Edwin is new to the business of sales and marketing
+	and is needing an easier way to get his product known.
+</p>
+<p>
+	<strong>Goals: </strong>Edwin's desire is to get people intrigued in his product in the easiest way possible
+	in hopes of expanding his customer range. He wants to find an efficient marketing service that will allow him
+	to have more time to focus on his artwork and personal brand.
+<p>
+	<strong>User Story: </strong>As a founder of a personal brand, I need a to showcase my brand through
+	video to reach a larger audience in order to increase my sales.
+</p>
 </body>
 </html>
