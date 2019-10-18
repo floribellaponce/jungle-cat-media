ca1d16bce38647b5ad2058e00baa267b5579ab76
Floribella Ponce <fponce2@cnm.edu>
2019-10-17 22:59:01 -0600

    added more information to persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -21,14 +21,26 @@ they can reach potential clients and take care of any of their videography needs
 <strong>Age: </strong> 29
 <strong>Profession: </strong>Owner of Slaytanic, a personal brand of merchandise consisting of clothing and
 accessories. He is looking to promote his product in order to reach a larger audience.
-<strong>Lifestyle: </strong> Edwin spends the majority of his time running his business. 
-<strong>Frustration and Needs: </strong> Edwin needs to expand his customer base. He has engaged in marketing
+<strong>Lifestyle: </strong> Edwin is a single father of a 5 year old child. He has background in graphic design
+and has taken a few courses in entrepreneurship. He started a solo personal brand a few months ago. He personally
+creates the graphics on his merchandise, manages the sales, and is responsible for the shipping and fulfillment.
+When he is not managing his business he spends time with his child and painting new artwork.
+<strong>Frustrations: </strong>
+<ul>
+	<<li>Hours spent on inefficient marketing techniques</li>
+	<li>Not having enough time to focus on creating new content for his personal brand</li>
+	<li>Customer base is not large enough to be substancial </li>
+		Edwin needs to expand his customer base. He has engaged in marketing
 strategies such as starting a social media account for his brand and showing his product to other businesses
 but it is has not been efficient for him. Edwin is new to the business of sales and marketing and is needing
 an easier way to get his product known. Edwin is running his business solo.
-<strong>Goals: </strong> Edwin is looking for a more effective marketing strategy to reach a larger
-population. Edwin is wanting to engage users in the easiest way possible to get customers
-intrigued in his product.
+<strong>Goals: </strong>
+	<ul>
+		<li>Reach a wider customer range </li>
+		<li>Get people intrigued in his product in the easier way possible </li>
+		<li>Find an efficient marketing tool</li>
+		<li>Have more time to create more content</li>
+	</ul>
 <strong>User Story: </strong>As a founder of a personal brand, I need a to showcase my brand through
 video to reach a larger audience in order to increase my sales.
 </body>
