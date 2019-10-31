33582d2411343ebf6ac95254fe05c2634d3d256f
Floribella Ponce <fponce2@cnm.edu>
2019-10-31 00:02:24 -0600

    listed website sections

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Milestone 2</title>
+</head>
+<body>
+<h1>Content Strategy</h1>
+<p>Purpose</p>
+<p>Video Slideshow</p>
+<p>Services</p>
+<p>About Us</p>
+<p>Testimonials</p>
+<p>Clips</p>
+<p>Contact</p>
+
+</body>
+</html>
