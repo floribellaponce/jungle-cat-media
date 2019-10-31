bf7782807148bfe9360c0425fe677aecefb5e09f
Floribella Ponce <fponce2@cnm.edu>
2019-10-31 06:57:46 -0600

    refined descriptions

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -6,19 +6,21 @@
 </head>
 <body bgcolor="#eee8aa">
 <h1>Content Strategy</h1>
-<p><strong>Purpose: </strong> he purpose section will display the companies purpose/mission statement. </p>
+<p><strong>Purpose: </strong> The purpose section will display the companies purpose/mission statement. The puspose section
+will have a background image behind the statement. </p>
 <p><strong>Video: </strong>The video section will have a video slide show embedded from a video playlist from a different site (Vimeo).
 	The slide show will display one video at a time and will have buttons on the right and left allowing users to view the other videos.
 	The videos will consist of videography done for businesses and personal brands. </p>
-<p><strong>Services: </strong>The services section will outline the services the company provide with icons to match the services.
-This section will only include two services but will eventually consist of more services the location can provide.</p>
+<p><strong>Services: </strong>The services section will outline the services the company provides with short description
+	and icons to match the services. This section will only include two services but will eventually consist of
+	more services the location can provide.</p>
 <p><strong>About Us: </strong>The about us section will have an image of the owner of the company, a personal statement/
 	summary about the business owner, and a personal statement/summary about the business.</p>
 <p><strong>Testimonials: </strong>The testimonial section will have statements from previous customers. This will be
 	a testimonial slide show that will display one testimonial at a time and will have a right and left button
 	allowing the user to see the next/previous testimonial. </p>
 <p><strong>Clips: </strong>The clips section will hold multiple clips from the companies Instagram page. The clips will
-	show a image of the content from the clip. When a user selects on the clip they will be directed to the
+	show a preview image of the content from the clip. When a user selects on the clip they will be directed to the
 	companies Instagram page where they will be able to see more content.</p>
 <p><strong>Contact: </strong>The contact section will consist of a contact form that will allow potential customers to
 	submit an inquiry. This section will also include the contact information of the business such as phone number and email. </p>
