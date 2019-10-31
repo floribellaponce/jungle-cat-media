5d54bf53ecc47c6d2e0e809c14d138578e6d28ec
Floribella Ponce <fponce2@cnm.edu>
2019-10-31 06:35:03 -0600

    added description of video section

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -7,8 +7,10 @@
 <body>
 <h1>Content Strategy</h1>
 <p><strong>Purpose:</strong> The purpose section will display the companies purpose/mission statement. </p>
-<p>Video Slideshow</p>
-<p>Services</p>
+<p><strong>Video: </strong>The video section will have a video slide show embedded from a video playlist from a different site (Vimeo).
+	The slide show will display one video at a time and will have buttons on the right and left allowing users to view the other videos.
+	The videos will consist of videography done for businesses and personal brands. </p>
+<p><strong>Services</strong></p>
 <p>About Us</p>
 <p>Testimonials</p>
 <p>Clips</p>
