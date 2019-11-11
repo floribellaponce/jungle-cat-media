7abc62ee367e8b60c1626b95a9abe28a53363391
Floribella Ponce <fponce2@cnm.edu>
2019-11-11 06:39:02 -0700

    added embeded video

--- php/index.php
+++ php/index.php
@@ -50,11 +50,17 @@
 		</ul>
 	</div>
 </header>
+<br>
 <!-- This is the purpose quote -->
-<blockquote class="blockquote text-center">
-	<p class="mb-0">"This is a place holder where the purpose of this business is. This will will be a quote from
-		the business owner himself explaining what the purpose of his business is."</p>
-</blockquote>
+<div class="container">
+	<blockquote class="blockquote text-center">
+		<p class="mb-0">"This is a place holder where the purpose of this business is. This will will be a quote from
+			the business owner himself explaining what the purpose of his business is."</p>
+	</blockquote>
+</div>
+<div class="container" align="center">
+	<iframe title="vimeo-player" src="https://player.vimeo.com/video/336812660"  width="450" height="360"  frameborder="0" allowfullscreen></iframe>
+</div>
 <!-- This is the testimonial carousel -->
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
 	<div class="carousel-inner">
@@ -101,12 +107,12 @@
 	<div class="row">
 		<div class="col-lg-6" align="center" >
 			<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-			<h2>Service #1</h2>
+			<h3>Service #1</h3>
 			<p>Very short description</p>
 		</div>
 		<div class="col-lg-6" align="center">
 			<svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 140x140"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
-			<h2>Service #1</h2>
+			<h3>Service #1</h3>
 			<p>Very short description</p>
 		</div>
 	</div>
